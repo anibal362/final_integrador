0bf495b6cb4f2426e967fdf07f17f93a3e8ef359
anibal362 <1021483@utp.edu.pe>
2017-08-04 03:54:25 -0500

    Add project files.

---
+++ Integrador_Reserva/Sources/Entities/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Integrador_Reserva.Sources.Entities
+{
+    public class Usuario
+    {
+        public int usuario_id { get; set; }
+        public string usuario_nombre { get; set; }
+        public string usuario_contraseña { get; set; }
+        public string usuario_razonsocial { get; set; }
+        public string usuario_ruc { get; set; }
+        public string usuario_email { get; set; }
+        public string usuario_telefono { get; set; }
+        public string usuario_foto_url { get; set; }
+        public string usuario_presentacion { get; set; }
+        public Boolean activo { get; set; }
+    }
+}
